d2a81daeb6d1495d756e2e6dbafe43c3569b59ba
Vadim Frolov <fralik@gmail.com>
2019-05-20 10:34:32 +0200

    Add exploratory target for paver

--- pavement.py
+++ pavement.py
@@ -6,24 +6,33 @@ from paver.setuputils import setup
 
 from src.data.download import pydownload_file
 from src.data.preprocess import pypreprocess
+from src.visualization.exploratory import pyexploratory
 
 iris_file = path('data/raw/iris.csv')
 preprocessed_file = path('data/processed/preprocessed.pickle')
+graph_file = path('reports/figures/exploratory.png')
 
 @task
 def rawdata():
-  """Download IRIS dataset"""
-  pydownload_file('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+    """Download IRIS dataset"""
+    pydownload_file('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
             iris_file)
 
 @task
 def clean():
-  """Clean the build"""
-  iris_file.remove()
-  preprocessed_file.remove()
+    """Clean the build"""
+    iris_file.remove()
+    preprocessed_file.remove()
+    graph_file.remove()
 
 @task
 @needs('rawdata')
 def preprocess():
-  """Preprocess IRIS dataset"""
-  pypreprocess(iris_file, preprocessed_file)
+    """Preprocess IRIS dataset"""
+    pypreprocess(iris_file, preprocessed_file)
+
+@task
+@needs('preprocess')
+def exploratory():
+    '''Make an image with pairwise distribution'''
+    pyexploratory(preprocessed_file, graph_file)
